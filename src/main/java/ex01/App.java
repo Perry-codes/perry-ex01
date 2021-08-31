0fc6695347af8fcafd7d90a0b4d0a5e6b8bc3d7d
McLovin4 <89555259+McLovin4@users.noreply.github.com>
2021-08-30 22:02:53 -0400

    Standard constraints met.

--- src/main/java/ex01/App.java
+++ src/main/java/ex01/App.java
@@ -1,11 +1,35 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Maggie Perry
+ */
+/*
+Set scanner to accept keyboard input
+
+Prompt user to enter their name
+
+Concatenate name into a welcome message string
+
+Output welcome message
+
+ */
+
 package ex01;
+
+import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("What is your name? ");
 
+        String userName = scanner.nextLine();
+
+        String welcomeMessage = "Hello, " + userName + ", nice to meet you!";
+
+        System.out.println(welcomeMessage);
 
     }
 }
